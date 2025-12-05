89106241a0d5515887dbc35fd2996d4c304d47d2
Harun Hursid <harun.hursid@hotmail.com>
2025-12-05 15:58:07 +0100

    er gået igang med mongo. er nået til mapping

---
+++ DiscProfilesApi/Interfaces/IGenericMongoRepository.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Driver;
+
+namespace DiscProfilesApi.Interfaces
+{
+    public interface IGenericMongoRepository<TDocument> where TDocument : class
+    {
+        Task<List<TDocument>> GetAllAsync();
+        Task<TDocument?> GetByIdAsync(int id);
+        Task<TDocument> InsertAsync(TDocument doc);
+        Task<bool> UpdateAsync(int id, TDocument doc);
+        Task<bool> DeleteAsync(int id);
+    
+
+    }
+}
---
+++ DiscProfilesApi/Repositories/GenericMongoRepository.cs
@@ -0,0 +1,53 @@
+﻿using DiscProfilesApi.Interfaces;
+using MongoDB.Driver;
+
+
+namespace DiscProfilesApi.Repositories
+{
+    public class GenericMongoRepository<TDocument> : IGenericMongoRepository<TDocument>
+    where TDocument : class
+    {
+
+        private readonly IMongoCollection<TDocument> _collection;
+
+        public GenericMongoRepository(IMongoDatabase database)
+        {
+            // Simpel convention: CompanyDocument -> "companies", EmployeeDocument -> "employees" osv.
+            var collectionName = typeof(TDocument).Name
+                .Replace("Document", "")
+                .ToLower() + "s";
+
+            _collection = database.GetCollection<TDocument>(collectionName);
+        }
+
+        public async Task<List<TDocument>> GetAllAsync() =>
+            await _collection.Find(Builders<TDocument>.Filter.Empty).ToListAsync();
+
+        public async Task<TDocument?> GetByIdAsync(int id)
+        {
+            var filter = Builders<TDocument>.Filter.Eq("Id", id);
+            return await _collection.Find(filter).FirstOrDefaultAsync();
+        }
+
+        public async Task<TDocument> InsertAsync(TDocument doc)
+        {
+            await _collection.InsertOneAsync(doc);
+            return doc;
+        }
+
+        public async Task<bool> UpdateAsync(int id, TDocument doc)
+        {
+            var filter = Builders<TDocument>.Filter.Eq("Id", id);
+            var result = await _collection.ReplaceOneAsync(filter, doc);
+            return result.ModifiedCount > 0;
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var filter = Builders<TDocument>.Filter.Eq("Id", id);
+            var result = await _collection.DeleteOneAsync(filter);
+            return result.DeletedCount > 0;
+        }
+
+    }
+}
